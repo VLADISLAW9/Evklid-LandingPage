8fba802054606159f0ff2141feb4059b037ccfe5
VLADISLAW9 <80681837+VLADISLAW9@users.noreply.github.com>
2022-10-03 22:40:18 +0700

    Evklid

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function () {
+    // OPEN MENU
+    document.querySelector("#burger").addEventListener("click", function () {
+        document.querySelector("#menu").classList.toggle("is-active")
+    })
+    // CLOSE MENU
+    document.querySelector("#close").addEventListener("click", function () {
+        document.querySelector("#menu").classList.toggle("is-active")
+    })
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+})
